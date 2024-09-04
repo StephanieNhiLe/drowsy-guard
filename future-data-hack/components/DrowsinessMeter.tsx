9740a419c9f3ab951d8313397c44c4a427b5045b
Stephanie Nhi Le <ledaquynhnhi@gmail.com>
2024-09-03 20:26:34 -0500

    visualize drowsinessMeter

--- future-data-hack/components/DrowsinessMeter.tsx
+++ future-data-hack/components/DrowsinessMeter.tsx
@@ -1,5 +1,6 @@
 import { useEffect, useState } from "react";
 import { useWebsocket, isWebsocketMessage } from "./contexts/websocketContext";
+import { View, Text, StyleSheet } from "react-native";
 
 const DrowsinessMeter = () => {
   const { lastMessage } = useWebsocket();
@@ -20,13 +21,58 @@ const DrowsinessMeter = () => {
       setDrowsinessLevel(drowsinessLevel);
     }
   }, [lastMessage]);
+
+  const getAlertText = (level: number) => {
+    if (level === 0) return "No Warning";
+    if (level === 1) return "Caution";
+    return "Warning: Drowsiness Detected";
+  };
+
+  const getAlertColor = (level: number) => {
+    if (level < 3) return styles.greenAlert;
+    if (level < 6) return styles.yellowAlert;
+    return styles.redAlert;
+  };
+
   return (
-    <>
-      {
-        // TODO: implement a real visual component for the drowsiness meter
-      }
-    </>
+    <View style={[styles.alertBox, getAlertColor(Number(drowsinessLevel))]}>
+    <Text style={styles.alertText}>{getAlertText(Number(drowsinessLevel))}</Text>
+    <Text style={styles.subText}>
+      {drowsinessLevel === 0 ? "No Drowsy Behaviors Detected" : "Drowsy Behaviors Detected"} 
+    </Text>
+      
+  </View>
   );
 };
+
+const styles = StyleSheet.create({
+  alertBox: {
+    width: '100%',
+    padding: 10,
+    borderRadius: 5,
+    borderColor: 'green',
+    borderWidth: 1,
+  },
+  greenAlert: {
+    backgroundColor: '#e6f9e7',
+    borderColor: 'green',
+  },
+  yellowAlert: {
+    backgroundColor: '#fff5cc',
+    borderColor: '#ffc107',
+  },
+  redAlert: {
+    backgroundColor: '#f8d7da',
+    borderColor: '#dc3545',
+  },
+  alertText: {
+    color: '#155724',
+    fontWeight: 'bold',
+  },
+  subText: {
+    color: '#155724',
+  }
+});
+
 
 export default DrowsinessMeter;
