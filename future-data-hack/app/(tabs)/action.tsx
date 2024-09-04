15fadc4c81b163a30a68a1fa899ede1074a780d8
AAyurzana <aayurzana@mastiffai.com>
2024-09-03 20:36:11 -0700

    nearest assisstance

--- future-data-hack/app/(tabs)/action.tsx
+++ future-data-hack/app/(tabs)/action.tsx
@@ -8,9 +8,23 @@ import MicrophoneComponent from "@/components/Microphone";
 import AudioStreamPlayer from "@/components/AudioStreamPlayer";
 
 export default function Action() {
+  const assisstances = [
+    {
+      isrecommended: true,
+      image: "Rest stop",
+      title: "Rest break recommended",
+      distance: "5 miles ahead",
+    },
+    {
+      isrecommended: false,
+      image: "Rest stop",
+      title: "Rest break recommended",
+      distance: "5 miles ahead",
+    },
+  ];
   return (
     <View style={{ padding: 10 }}>
-      <View style={{ display: "flex" }}>
+      <View style={{ display: "flex", flexDirection: "row", marginBottom: 20 }}>
         <View
           style={{
             height: 50,
@@ -21,11 +35,68 @@ export default function Action() {
           }}
         />
         <View>
-          <Text style={{ fontWeight: "bold" }}>Driver's Assisstant</Text>
+          <Text style={{ fontWeight: "bold", fontSize: 20 }}>
+            Driver's Assisstant
+          </Text>
           <Text>Assessing Driver Alertness</Text>
         </View>
       </View>
       <DrowsinessMeter />
+      <View style={{ marginTop: 20 }}>
+        <Text style={{ fontSize: 20, marginBottom: 10 }}>
+          {" "}
+          Nearest Assistance
+        </Text>
+        <View style={{ display: "flex", flexDirection: "row" }}>
+          {assisstances.map((as) => {
+            return (
+              <View
+                style={{
+                  borderRadius: 10,
+                  position: "relative",
+                  borderColor: "grey",
+                  width: 200,
+                  height: 300,
+                  borderWidth: 1,
+                  marginRight: 20,
+                  overflow: "hidden",
+                }}
+              >
+                {as.isrecommended && (
+                  <Text
+                    style={{
+                      position: "absolute",
+                      top: 5,
+                      left: 5,
+                      color: "black",
+                      zIndex: 1,
+                    }}
+                  >
+                    Recommended
+                  </Text>
+                )}
+                <View
+                  style={{
+                    backgroundColor: "grey",
+                    height: 200,
+                    display: "flex",
+                    alignItems: "center",
+                    justifyContent: "center",
+                  }}
+                >
+                  <Text>{as.image}</Text>
+                </View>
+                <View style={{ padding: 10 }}>
+                  <Text style={{ marginBottom: 10 }}>{as.title}</Text>
+                  <Text style={{ fontSize: 20, fontWeight: "bold" }}>
+                    {as.distance}
+                  </Text>
+                </View>
+              </View>
+            );
+          })}
+        </View>
+      </View>
     </View>
   );
 }
